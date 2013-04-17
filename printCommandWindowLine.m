845b0ad45ca5fa167c0f1fcd7f683ec7b4880a19
Sebastian Eicke <sebastianeicke@web.de>
2013-04-17 11:19:31 +0200

    Commit printCommandWindowsLine

---
+++ printCommandWindowLine.m
@@ -0,0 +1,10 @@
+%% printCommandWindowLine.m Print line in Command Window
+%  Author:   Sebastian Eicke
+%  Date:     28. January 2012
+%  Version:  12.01.28.16
+%
+%% Code
+function [] = printCommandWindowLine()
+
+cws = get(0, 'CommandWindowSize');
+fprintf(strcat(char(ones(1,cws(1)).*45),'\n'));
