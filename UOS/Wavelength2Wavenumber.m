d299c27ffa48aceb37ce5c4b17d8cc73e64933fe
Sebastian Eicke <sebastianeicke@web.de>
2013-04-24 20:48:28 +0200

    Add initial version of conversion scripts for wavelength and wavenumbers

---
+++ UOS/Wavelength2Wavenumber.m
@@ -0,0 +1,16 @@
+%% Wavelength2Wavenumber.m
+%
+%  Author:   Sebastian Eicke (sebastianeicke@web.de)
+%  Date:     06. July 2011
+%  Version:  11.07.06.16
+%
+%% Input
+%	 lambda  Wavelength (nm)
+%
+%% Output:
+%    nu      Wavenumber (cm^-1)
+%
+%% Code
+function [nu] = Wavelength2Wavenumber(lambda)
+
+nu = 1./(lambda.*1e-9.*100);
---
+++ UOS/Wavenumber2Wavelength.m
@@ -0,0 +1,16 @@
+%% Wavelength2Wavenumber.m
+%
+%  Author:   Sebastian Eicke (sebastianeicke@web.de)
+%  Date:     16. July 2011
+%  Version:  11.07.16.11
+%
+%% Input
+%	 nu      Wavenumber (cm^-1)
+%
+%% Output:
+%    lambda  Wavelength (nm)
+%
+%% Code
+function [lambda] = Wavenumber2Wavelength(nu)
+
+lambda = 1./(nu.*1e-9.*100);
